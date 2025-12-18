d7887d4b5e4b62f840779bfcc250a4e6435ae45f
alpana15-mix <aalpana48@gmail.com>
2025-12-18 16:05:39 +0530

    I have partial done the part of the home page

--- you-tube/src/components/HomePage.jsx
+++ you-tube/src/components/HomePage.jsx
@@ -5,13 +5,13 @@ const Options = ["All","4K resolution","JavaScript","Data Structure","Mixes","Mu
 
     return(
        
-        <div className={navbar?" nav flex text-white flex-col overflow-x-auto flex-1 ml-[247px] min-h-screen":"nav flex text-white flex-col overflow-x-auto flex-1 ml-[77px] min-h-screen"}>
-            <div className="flex nav text-white fixed top-15 z-1 w-full box-border gap-2.5 shrink-0 h-auto overflow-x-scroll bg-black">
+        <div className={navbar?" nav  flex text-white flex-col overflow-x-auto flex-1 ml-[247px] min-h-screen":"nav flex text-white flex-col overflow-x-auto flex-1 ml-[77px] min-h-screen"}>
+            <div className="flex nav shadow-md shadow-gray-700 text-white fixed top-15 z-1 w-full box-border gap-2.5 shrink-0 h-auto overflow-x-scroll bg-black">
 {/*---------------------------------Options--------------------------------------------*/}
                 {
                     Options.map((item,index)=>{
                         return(
-                            <div key={index} className="flex shrink-0 h-[30px] px-2.5 py-px bg-[rgb(42,42,42)] text-white font-semibold rounded-[5px] items-center justify-center cursor-pointer hover:bg-[rgb(60,60,60) transition]">
+                            <div key={index} className="flex shrink-0 h-[30px] px-2.5 py-px bg-[rgb(42,42,42)] text-white font-semibold rounded-[5px] items-center justify-center cursor-pointer hover:bg-[rgb(60,60,60) transition ]">
                                  {item}
                              </div>
                         );
@@ -22,6 +22,36 @@ const Options = ["All","4K resolution","JavaScript","Data Structure","Mixes","Mu
                   
            
             </div>
+
+            {/*-----------------------------Video_Part----------------------------*/}
+
+<div className='main_home_page grid box-border bg-black gap-2.5 grid-cols-[384px_384px_384px] pt-[90px] pb-5'>
+
+                <div className='you_tube_video flex flex-col box-border text-white no-underline cursor-pointer h-[316px] hover:scale-[1.02] transition-transform duration-200'>
+
+                        <div className='thumnail relative box-border h-[216px]'>
+                            <img src='https://tinypng.com/images/social/website.jpg' alt='thumnail' className='w-full h-full rounded-[10px]'/>
+                            <div className='video-timing absolute bottom-0 right-0 w-auto px-0.5 py-px bg-[rgb(42,42,42)] rounded-[5px]'>
+                                12:32
+                            </div>
+
+                        </div>
+                        <div className='title-box '>
+                            <div className='profile_image flex w-[50px] h-[50px] items-center justify-center'>
+                                <img src='https://tinypng.com/images/social/website.jpg' alt='profile' className='thumnail_profile w-[80%] rounded-full object-cover'/>
+                            </div>
+                            <div className='video_title w-full p-[5px] box-border flex gap-3'>
+                                <div className='title font-semibold text-sm leading-snug line-clamp-2'>User1</div>
+                                 <div className='channel text-sm text-gray-400 mt-1'>User1</div>
+                                  <div className='likes text-sm text-gray-400 mt-1'>User1</div>
+                                   
+                            </div>
+                        </div>
+
+                </div>
+
+</div>
+
        
          </div>
             
--- you-tube/src/components/Sidebar.jsx
+++ you-tube/src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ import { MdContentCut } from "react-icons/md";
 import '../index.css' 
 export default function Sidebar({navbar}){
     return(
-       <>{navbar? <div className="sidebar flex flex-col grow box-border h-[92vh] overflow-y-auto fixed top-[55px] left-0 w-[260px] p-3.5 bg-black text-white"
+       <>{navbar? <div className="sidebar shadow-[4px_0_10px_rgba(0,0,0,0.9)] flex flex-col grow box-border h-[92vh] overflow-y-auto fixed top-[55px] left-0 w-[260px] p-3.5 bg-black text-white"
             >
             {/*First container*/}
             <div className="flex flex-col border-b border-[#7e5959] pb-2.5">
@@ -115,7 +115,7 @@ export default function Sidebar({navbar}){
 
  </div>:
  
- <div className="flex flex-col grow box-border h-[92vh] overflow-y-auto fixed top-[55px] left-0 w-[90px] p-3.5 bg-black text-white"
+ <div className="flex shadow-[4px_0_10px_rgba(0,0,0,0.9)] flex-col grow box-border h-[92vh] overflow-y-auto fixed top-[55px] left-0 w-[90px] p-3.5 bg-black text-white"
             >
             {/*First container*/}
             <div className="sidebar flex flex-col border-b border-[#7e5959] pb-2.5">
